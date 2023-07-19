e8895d3a36860e6030b3acfff80d5f9c0d24456d
David Timofeev <dava.tim@yandex.ru>
2023-07-19 22:43:03 +0300

    ПР 14 v1

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     // required: [true, 'Заполните поле'],
     minlength: [2, 'Минимальная длина - 2 символа'],
     maxlength: [30, 'Максимальная длина - 30 символов'],
-    default: 'Жак Ив Кусто',
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
