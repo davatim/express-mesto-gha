ba806f9b2aa6482b16f40d447d29f12665ace9f4
David Timofeev <dava.tim@yandex.ru>
2023-07-20 18:01:19 +0300

    ПР 14 v6.7

--- controllers/cards.js
+++ controllers/cards.js
@@ -44,8 +44,7 @@ module.exports.deleteCard = (req, res, next) => {
       if (owner === user) {
         return Card.deleteOne(card)
           .then(() =>
-            // return next(new INFO_200_SEC_SEND('Карточка с данными удалена'));
-            res.status(INFO_200_SEC_SEND).send({ message: 'Карточка с данными удалена' }));
+            return res.status(INFO_200_SEC_SEND).send({ message: 'Карточка с данными удалена' }));
       }
       return next(new ERROR_403_PERMISSION('У вас нет прав для этого'));
     })
